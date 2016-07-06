c68d64f3c5f51ef031900c0e149aa78dcc531862
Wica <WicaMD@users.noreply.github.com>
2016-07-06 15:43:57 +0200

    Update Program.cs

--- WatermarkDisabler/Program.cs
+++ WatermarkDisabler/Program.cs
@@ -1,14 +1,3 @@
-using System;
-using System.Linq;
-using System.Runtime.InteropServices;
-using EloBuddy;
-using EloBuddy.SDK;
-using EloBuddy.SDK.Events;
-using EloBuddy.SDK.Menu;
-using EloBuddy.SDK.Menu.Values;
-using Color = System.Drawing.Color;
-using SharpDX;
-
 namespace WatermarkDisabler
 {
     class Program
